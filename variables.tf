27fec003b2cb2193680be6e406f56e1e18f0f710
Shirish Umredkar <shirish.umredkar@gmail.com>
2026-07-18 11:06:05 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,33 @@
-variable "project_id"        { type = string }
-variable "region"            { type = string; default = "us-central1" }
-variable "vpc_connector_id"  { type = string }
-variable "kms_key_id"        { type = string }
-variable "cloudrun_sa_email" { type = string }
-variable "registry_url"      { type = string }
-variable "image_tag"         { type = string; default = "latest" }
+variable "project_id" {
+  type    = string
+  default = "project-495bdca4-ac50-4df5-bb6"
+}
+variable "region"     { 
+   type = string
+   default = "us-central1" 
+}
+variable "vpc_connector_id"     { 
+   type = string
+   default = "us-central1" 
+}
+
+variable "kms_key_id"     { 
+   type = string
+   default = "us-central1" 
+}
+
+variable "cloudrun_sa_email"     { 
+   type = string
+   default = "us-central1" 
+}
+
+variable "registry_url"     { 
+   type = string
+   default = "us-central1" 
+}
+
+variable "image_tag"     { 
+   type = string
+   default = "latest" 
+}
+
